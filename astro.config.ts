c2f760407ce6aaac9b00356743efbd9d96d4f1df
Daniel Smedema <djsmedema@wisc.edu>
2025-06-29 18:53:56 -0500

    attempt

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,7 @@
 import { defineConfig } from "astro/config";
+import deno from "@deno/astro-adapter";
 
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: deno(),
+});
